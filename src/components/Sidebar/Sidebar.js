1b633c40f0d15dd5f72c06eb22ae26f7df84e9bb
hiba247 <hi24ba07@gmail.com>
2023-09-06 10:49:01 +0100

    first commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "react";
+import { Drawer, IconButton, List } from "@material-ui/core";
+import {
+  Home as HomeIcon,
+  NotificationsNone as NotificationsIcon,
+  FormatSize as TypographyIcon,
+  FilterNone as UIElementsIcon,
+  FileCopy as File,
+  SupervisorAccount as SupervisorAccount,
+  BorderAll as TableIcon,
+  BarChart as Dashboard,
+  ListAlt as ListAlt,
+  QuestionAnswer as SupportIcon,
+  LibraryBooks as LibraryIcon,
+  HelpOutline as FAQIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@material-ui/icons";
+import { useTheme } from "@material-ui/styles";
+import { withRouter } from "react-router-dom";
+import classNames from "classnames";
+import Theme from "./Theme";
+// styles
+import useStyles from "./styles";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+// components
+import SidebarLink from "./components/SidebarLink/SidebarLink";
+import Dot from "./components/Dot";
+
+// context
+import {
+  useLayoutState,
+  useLayoutDispatch,
+  toggleSidebar,
+} from "../../context/LayoutContext";
+
+import user from "../../Icons/user.png";
+
+const structure = [
+  { id: 0, label: "Statistiques", link: "/app/dashboard", icon: <Dashboard /> },
+ ,
+  { id: 1, label: "Projets", link: "/app/tables", icon: <ListAlt /> },
+  {
+    id: 2,
+    label: "Documents",
+    link: "/app/notifications",
+    icon: <File />,
+  },
+  {
+    id: 3,
+    label: "Notifications",
+    link: "/app/ui",
+    icon: <NotificationsIcon />,
+    children: [
+      { label: "Icons", link: "/app/ui/icons" },
+      { label: "Charts", link: "/app/ui/charts" },
+      { label: "Maps", link: "/app/ui/maps" },
+    ],
+  },
+  {
+    id: 4,
+    label: "Utilisateurs",
+    link: "/app/ui",
+    icon: <SupervisorAccount/>,
+    children: [
+      { label: "Icons", link: "/app/ui/icons" },
+      { label: "Charts", link: "/app/ui/charts" },
+      { label: "Maps", link: "/app/ui/maps" },
+    ],
+  },
+  {
+    id: 5,
+    label: "Acceuil",
+    link: "/app/ui",
+    icon: <HomeIcon />,
+    children: [
+      { label: "Icons", link: "/app/ui/icons" },
+      { label: "Charts", link: "/app/ui/charts" },
+      { label: "Maps", link: "/app/ui/maps" },
+    ],
+  },
+  { id: 6, type: "divider" },
+  ,
+
+];
+
+function Sidebar({ location }) {
+  var classes = useStyles();
+
+  // global
+  var { isSidebarOpened } = useLayoutState();
+  var layoutDispatch = useLayoutDispatch();
+
+  // local
+  var [isPermanent, setPermanent] = useState(true);
+
+  useEffect(function() {
+    window.addEventListener("resize", handleWindowWidthChange);
+    handleWindowWidthChange();
+    return function cleanup() {
+      window.removeEventListener("resize", handleWindowWidthChange);
+    };
+  });
+
+  return (
+<ThemeProvider theme={Theme}>
+
+    <Drawer  
+
+      variant={isPermanent ? "permanent" : "temporary"}
+      className={classNames(classes.drawer, {
+        [classes.drawerOpen]: isSidebarOpened,
+        [classes.drawerClose]: !isSidebarOpened,
+      })}
+      classes={{
+        paper: classNames({
+          [classes.drawerOpen]: isSidebarOpened,
+          [classes.drawerClose]: !isSidebarOpened,
+        }),
+      }}
+      open={isSidebarOpened}
+    >
+      <div className={classes.toolbar} />
+      <div className={classes.mobileBackButton}>
+        <IconButton onClick={() => toggleSidebar(layoutDispatch)}>
+          <ArrowBackIcon
+            classes={{
+              root: classNames(classes.headerIcon, classes.headerIconCollapse),
+            }}
+          />
+        </IconButton>
+      </div>
+      <List className={classes.sidebarList}>
+        {structure.map(link => (
+          <SidebarLink
+            key={link.id}
+            location={location}
+            isSidebarOpened={isSidebarOpened}
+            {...link}
+          />
+        ))}
+      </List>
+    </Drawer>
+</ThemeProvider>
+  );
+
+  // ##################################################################
+  function handleWindowWidthChange() {
+    var windowWidth = window.innerWidth;
+    var breakpointWidth = Theme.breakpoints.values.md;
+    var isSmallScreen = windowWidth < breakpointWidth;
+
+    if (isSmallScreen && isPermanent) {
+      setPermanent(false);
+    } else if (!isSmallScreen && !isPermanent) {
+      setPermanent(true);
+    }
+  }
+}
+
+export default withRouter(Sidebar);
