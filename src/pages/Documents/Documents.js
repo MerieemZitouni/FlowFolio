3a3c7d09baf74c2036783118ad72b85512136c70
Merieem.Zitouni <km_zitouni@esi.dz>
2023-09-09 18:45:30 +0100

    add doc buttons

--- src/pages/Documents/Documents.js
+++ src/pages/Documents/Documents.js
@@ -1,12 +1,15 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+import React ,{useState}from "react";
+import { Button, Grid ,Popover, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import MUIDataTable from "mui-datatables";
-
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"; // Import the arrow icon
+import { FilterList } from "@material-ui/icons";
+import { Add } from "@material-ui/icons";
 // components
 import PageTitle from "../../components/PageTitle/PageTitle";
 import Widget from "../../components/Widget/Widget";
 import Table from "../dashboard/components/Table/Table";
+import Stack from '@mui/material/Stack';
 
 // data
 import mock from "../dashboard/mock";
@@ -121,37 +124,208 @@ const documents=
    
   ]}
 
-const useStyles = makeStyles(theme => ({
-  tableOverflow: {
-    overflow: 'auto'
+  function ActionsDropdown({ rowData, handleDelete, handleEdit, handleHistory, handleComments, handleAddRevision }) {
+    const [anchorEl, setAnchorEl] = useState(null);
+  
+    const handlePopoverOpen = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handlePopoverClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const open = Boolean(anchorEl);
+  
+    return (
+      <div>
+        <IconButton
+          aria-label="Actions"
+          onClick={handlePopoverOpen}
+        >
+          <ArrowDropDownIcon />
+        </IconButton>
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handlePopoverClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <div>
+            <button onClick={() => handleDelete(rowData)}>Supprimer</button>
+            <button onClick={() => handleEdit(rowData)}>Modifier</button>
+            <button onClick={() => handleHistory(rowData)}>Historique</button>
+            <button onClick={() => handleComments(rowData)}>Commentaires</button>
+            <button onClick={() => handleAddRevision(rowData)}>Ajouter Nouvelle Révision</button>
+          </div>
+        </Popover>
+      </div>
+    );
   }
-}))
-
-const options = {
-  filter: false,
-  rowsPerPage: [10],
-  textLabels: {
-    pagination: {
-      next: "Next >",
-      previous: "< Previous",
-      rowsPerPage: "Total items Per Page",
-      displayRows: "OF",
+  
+  const columns = [
+    // ... (other columns)
+    {name: "Titre",
+    label: "Titre",
+    options: {
+      filter: true,
+      sort: true,
+      search: true, // Enable search for this column
     },
   },
-  onChangePage(currentPage) {
-    console.log({ currentPage });
+  {
+    name: "Code",
+    label: "Code",
+    options: {
+      filter: true,
+      sort: true,
+      search: true, // Enable search for this column
+    },
   },
-  onChangeRowsPerPage(numberOfRows) {
-    console.log({ numberOfRows });
+  {
+    name: "Révision Actuelle",
+    label: "Révision Actuelle",
+    options: {
+      filter: true,
+      sort: true,
+      search: true, // Enable search for this column
+    },
   },
-};
+  {
+    name: "Type",
+    label: "Type",
+    options: {
+      filter: true,
+      sort: true,
+      search: true, // Enable search for this column
+    },
+  },
+  {
+    name: "Statut",
+    label: "Statut",
+    options: {
+      filter: true,
+      sort: true,
+      search: true, // Enable search for this column
+    },
+  },
+     { name: "Actions",
+      label: "Actions",
+      options: {
+        filter: false,
+        sort: false,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          const rowData = tableMeta.rowData;
+          return (
+            <ActionsDropdown
+              rowData={rowData}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+              handleHistory={handleHistory}
+              handleComments={handleComments}
+              handleAddRevision={handleAddRevision}
+            />
+          );
+        },
+      },
+    },
+  ];
+  
+  const useStyles = makeStyles(theme => ({
+    tableOverflow: {
+      overflow: 'auto'
+    },
+    button:{
+      backgroundColor: "#3A85F4",
+      borderRadius: "30px",
+      textTransform: "capitalize",
+      fontFamily: "Poppins",
+    },
+    stackButtons:{
+      marginBottom: theme.spacing(2),
+      marginTop: theme.spacing(2),
+      justifyContent: 'flex-end',
+    }
+  }))
+  
+  const options = {
+    download: false, // Remove download option
+    print: false, // Remove print option
+    selectableRows: "none", // Remove checkbox selection
+    filter: false,
+    search: true, // Enable global search
+    rowsPerPage: [10],
+    textLabels: {
+      pagination: {
+        next: "Next >",
+        previous: "< Previous",
+        rowsPerPage: "Total items per page",
+        displayRows: "of",
+      },
+    },
+    onChangePage(currentPage) {
+      console.log({ currentPage });
+    },
+    onChangeRowsPerPage(numberOfRows) {
+      console.log({ numberOfRows });
+    },
+  };
+  
+  function handleDelete(rowData) {
+    // Implement delete action here using the rowData
+    console.log("Delete clicked for row: ", rowData);
+  }
+  
+  function handleEdit(rowData) {
+    // Implement edit action here using the rowData
+    console.log("Edit clicked for row: ", rowData);
+  }
+  
+  function handleHistory(rowData) {
+    // Implement history action here using the rowData
+    console.log("History clicked for row: ", rowData);
+  }
+  
+  function handleComments(rowData) {
+    // Implement comments action here using the rowData
+    console.log("Comments clicked for row: ", rowData);
+  }
+  
+  function handleAddRevision(rowData) {
+    // Implement add revision action here using the rowData
+    console.log("Add Revision clicked for row: ", rowData);
+  }
+  
+  export default function Documents() {
+    const classes = useStyles();
+    return (
+      <>
+        <Stack spacing={2} direction="row" className={classes.stackButtons}>
+      <Button
+      variant="contained"
+      color = "primary"
+      startIcon={<FilterList />}
+      className={classes.button}
+    >
+      Filtrer
+    </Button>
+    <Button
+      variant="contained"
+      color = "primary"
+      startIcon={<Add />}
+      className={classes.button}
+    >
+      Ajouter
+    </Button>
+    </Stack>
 
-
-export default function Documents() {
-  const classes = useStyles();
-  return (
-    <>
-      <PageTitle title="Tables" />
       <Grid container spacing={4}>
         <Grid item xs={12}>
           <MUIDataTable
@@ -164,5 +338,5 @@ export default function Documents() {
        
       </Grid>
     </>
-  );
-}
+    );
+  }
